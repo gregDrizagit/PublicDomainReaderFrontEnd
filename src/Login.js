273ec37e3f987678ee1023980b4b4c857768ccb5
github username <github email address>
2018-03-10 14:12:01 -0500

    logged in as cannonball

--- src/Login.js
+++ src/Login.js
@@ -11,7 +11,7 @@ class Login extends React.Component{
     super()
     this.state = {
       errors:false,
-      username: "sstitt",
+      username: "jadderley",
       password: "123"
     }
   }
